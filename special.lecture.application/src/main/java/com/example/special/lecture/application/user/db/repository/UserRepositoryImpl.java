fe1f87c725bce6731fe3e760a99ce8bedf45f62f
Taehwan2 <taehwansw@gmail.com>
2024-06-27 19:58:09 +0900

    [user] 테스트와 기능 추가

--- special.lecture.application/src/main/java/com/example/special/lecture/application/user/db/repository/UserRepositoryImpl.java
+++ special.lecture.application/src/main/java/com/example/special/lecture/application/user/db/repository/UserRepositoryImpl.java
@@ -1,16 +1,24 @@
 package com.example.special.lecture.application.user.db.repository;
 
 import com.example.special.lecture.application.user.db.UserEntity;
+import com.example.special.lecture.application.user.service.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.NoSuchElementException;
+
 @Repository
 @RequiredArgsConstructor
-public class UserRepositoryImpl implements UserRepository{
+public class UserRepositoryImpl implements UserRepository {
     private final UserJpaRepository userJpaRepository;
 
     @Override
     public UserEntity post(UserEntity user) {
         return userJpaRepository.save(user);
+    }
+
+    @Override
+    public UserEntity findUserByUserId(Long userId) {
+        return userJpaRepository.findById(userId).orElseThrow(()->  new NoSuchElementException("사용자가 없습니다."));
     }
 }
