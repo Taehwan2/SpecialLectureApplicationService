57ceb6555a1547e9703050d318db7f37cb0dd64a
Taehwan2 <taehwansw@gmail.com>
2024-06-27 22:56:02 +0900

    기능 구현 완료 -> 동시성 테스트만 남음

---
+++ special.lecture.application/src/main/java/com/example/special/lecture/application/apply/controller/LectureController.java
@@ -0,0 +1,41 @@
+package com.example.special.lecture.application.apply.controller;
+
+import com.example.special.lecture.application.Lecture.controller.model.LectureResponse;
+import com.example.special.lecture.application.Lecture.db.Lecture;
+import com.example.special.lecture.application.apply.controller.model.ApplyRequest;
+
+import com.example.special.lecture.application.apply.facade.ApplyFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/lectures")
+public class LectureController {
+
+    private final ApplyFacade applyService;
+
+    @GetMapping("/{userId}/{lectureId}")
+    public  Boolean getApplyHistory(@PathVariable(name = "userId") Long userId,@PathVariable(name = "lectureId")Long lectureId) {
+        return applyService.checkUserApplyLecture(userId,lectureId);
+    }
+
+    @GetMapping("")
+    public  List<LectureResponse> getLectures() {
+        return applyService.findAllLectures().stream()
+                .map(Lecture::entityToResponse)
+                .collect(Collectors.toList());
+    }
+
+
+    @PatchMapping("")
+    public Boolean applySpecialLecture(@RequestBody  ApplyRequest applyRequest) throws IllegalAccessException {
+        return applyService.applySpecialLecture(applyRequest);
+    }
+}
--- special.lecture.application/src/test/java/com/example/special/lecture/application/controller/model/LectureResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.special.lecture.application.controller.model;
-
-public class LectureResponse {
-}
